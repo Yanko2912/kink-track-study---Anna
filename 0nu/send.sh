54c66e7c69b08b95d706eecf7869db90391eb623
Anna Yanko <ann.yankoooo@gmail.com>
2025-08-05 18:02:38 +0200

    changed reco.conf

--- 0nu/send.sh
+++ 0nu/send.sh
@@ -9,18 +9,20 @@
 #SBATCH --time=50:00                 	 # Time for the job in format “minutes:seconds” or  “hours:minutes:seconds”, “days-hours”
 #SBATCH --cpus-per-task=1                # Number of CPUs
 
+source ${THRONG_DIR}/config/supernemo_profile.bash
+snswmgr_load_setup falaise@5.1.5
 
 FALAISE_BIN=/sps/nemo/sw/redhat-9-x86_64/snsw/opt2/falaise-5.1.5/bin
 CONF_FAL=/sps/nemo/sw/Falaise/install_develop/share/Falaise-4.1.0/resources/snemo/demonstrator/reconstruction
 WORKDIR=/sps/nemo/scratch/ayanko/kink_track_study_Anna/0nu
-SNCUTS_CONF=/sps/nemo/scratch/ayanko/software/SNCuts/build/SNCutsPipeline.conf
+SNCUTS_CONF=/sps/nemo/scratch/ayanko/kink_track_study_Anna/0nu/
 
-$FALAISE_BIN/flsimulate -c $WORKDIR/simu_setup.conf -o $WORKDIR/simu_setup.brio
+$FALAISE_BIN/flsimulate -c $WORKDIR/simu_setup.conf -o $WORKDIR/simu.brio
 
-$FALAISE_BIN/flreconstruct -i $WORKDIR/simu_setup.brio -p $CONF_FAL/official-2.0.0.conf -o $WORKDIR/reco.brio
+#$FALAISE_BIN/flreconstruct -i $WORKDIR/simu_setup.brio -p $CONF_FAL/official-2.0.0.conf -o $WORKDIR/reco.brio
 
-$FALAISE_BIN/flreconstruct -i $WORKDIR/reco.brio -p reco.conf -o $WORKDIR/reco2.brio
+$FALAISE_BIN/flreconstruct -i $WORKDIR/simu.brio -p reco.conf -o $WORKDIR/reco.brio
 
-$FALAISE_BIN/flreconstruct -i $WORKDIR/reco2.brio -p $SNCUTS_CONF -o $WORKDIR/CDFiltered.brio
+#$FALAISE_BIN/flreconstruct -i $WORKDIR/reco.brio -p $WORKDIR/SNCutsPipeline.conf -o $WORKDIR/reco_cuts.brio
 # /sps/nemo/sw/redhat-9-x86_64/snsw/opt2/falaise-5.1.5/bin/flreconstruct -i /pbs/home/a/ayanko/hihihaha/pb/Reco_pb1000.brio -p /pbs/home/a/ayanko/private/MiModule/p_MiModule_v00.conf
 
