416a6dfa2940d0180e00207fd40d35a10a887c9f
Anna Yanko <ann.yankoooo@gmail.com>
2025-08-08 14:10:19 +0200

    chanded manager.sh for two values

--- 0nu/manager.sh
+++ 0nu/manager.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+  exit 1
+fi
+
+start=$1
+end=$2
+
 mkdir -p DATA
 
-for ((i = 0; i < $1; i++)); do
+for ((i = start; i <= end; i++)); do
   mkdir -p "DATA/$i"
-cp simu_setup.conf  "DATA/$i/"
+  cp simu_setup.conf  "DATA/$i/"
   cp reco.conf        "DATA/$i/"
   cp pipeline.conf    "DATA/$i/"
-	sbatch \
-	--output="DATA/$i/slurm-%j.out" \
-	send.sh "$i"
-
-
+  sbatch \
+    --output="DATA/$i/slurm-%j.out" \
+    send.sh "$i"
 done
